a8537960919047fe28c02dcdf83f396e612f93cc
Akshay Gupta <kitallis@gmail.com>
2014-07-31 16:44:43 +0530

    bumping up a version, signing my jar

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cahoot/lein-template "0.1.5"
+(defproject cahoot/lein-template "0.1.6"
   :description "dependencies and macros to help bootstrap your clojure application"
   :url "https://github.com/kitallis/cahoot"
   :license {:name "Eclipse Public License"
