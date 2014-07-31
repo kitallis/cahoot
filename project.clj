758b6d2757e39b47d519c8a46a3f1e0ab513a081
Akshay Gupta <kitallis@gmail.com>
2014-07-31 15:49:06 +0530

    bumping up the version and adding log4j properties

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cahoot/lein-template "0.1.3"
+(defproject cahoot/lein-template "0.1.4"
   :description "dependencies and macros to help bootstrap your clojure application"
   :url "https://github.com/kitallis/cahoot"
   :license {:name "Eclipse Public License"
--- src/leiningen/new/cahoot.clj
+++ src/leiningen/new/cahoot.clj
@@ -11,14 +11,14 @@
               :sanitized (name-to-path name)}]
     (main/info "Generating fresh 'lein new' cahoot project.")
     (->files data
-             ["bin/"]
-             ["resources/"]
-             ["src/{{name}}/"]
-             ["test/{{name}}/"]
-             ["server-start" (render "bin/server-start")]
+             "bin/"
+             "resources/"
+             "src/{{name}}/"
+             "test/{{name}}/"
+             ["bin/server-start" (render "bin/server-start")]
              ["src/{{sanitized}}/db.clj" (render "db.clj")]
              ["src/{{sanitized}}/web.clj" (render "web.clj")]
-             ["log4j.properties" (render "resources/log4j.properties")]
+             ["resources/log4j.properties" (render "resources/log4j.properties")]
              ["src/{{sanitized}}/handler.clj" (render "handler.clj")]
              ["test/{{sanitized}}/test_helper.clj" (render "test_helper.clj")]
              ["Procfile" (render "Procfile")]
